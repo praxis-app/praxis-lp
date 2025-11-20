130a996654da242f695ff79afe7d0647853ceea6
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-11-20 12:26:26 -0500

    chore: add tw css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1>Praxis LP</h1>
+        <h1 className="text-4xl font-bold text-center mt-20">Praxis LP</h1>
       </main>
     </div>
   );
