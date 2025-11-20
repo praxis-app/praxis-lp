a042d3d3b1286d55c95c62dccdbb616824af044a
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-11-20 16:47:46 -0500

    chore: apply formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1 className="text-4xl font-bold text-center mt-20">Praxis LP</h1>
+        <h1 className="mt-20 text-center text-4xl font-bold">Praxis LP</h1>
       </main>
     </div>
   );
